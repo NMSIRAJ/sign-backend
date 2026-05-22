0d28e232b284429e35ba4e0d25f5ae93a0aed076
NMSIRAJ <noredin2012@gmail.com>
2026-05-22 12:07:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-const OpenAI = require("openai");
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-});
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
